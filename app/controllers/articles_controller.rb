600a484be837bf052a531d99fbd98d8f4d2ed591
Kade Illian <kadeillianmt@gmail.com>
2022-09-14 11:36:17 -0600

    created articles controller with index action the renders top headlines from newsapi.org.  Also encrytped my news_api_key in my credentials file and made that work with my index file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,7 @@
 class ArticlesController < ApplicationController
+  def index
+    response = HTTP.get("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=#{Rails.application.credentials.news_api_key}")
+
+    render json: response.parse(:json)["articles"]
+  end
 end
